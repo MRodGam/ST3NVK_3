799e2b02e27b0ccee584c7e176335d1fc5ba39cb
Matilde Gammelgaard <matildegammelgaard@hotmail.com>
2018-12-03 12:56:00 +0100

    Oprettet projekt

---
+++ NVK3_RestfulCLient/NVK3_RestfulCLient/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NVK3_RestfulCLient
+{
+    public class Class1
+    {
+    }
+}
